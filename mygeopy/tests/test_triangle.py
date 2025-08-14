1c20644e98af32ea367b71b5ca01e3e6ecb75017
jiasquaredfu <jjnaturalist@gmail.com>
2025-08-14 14:41:32 -0800

    oops last commit did not stage

--- mygeopy/tests/test_triangle.py
+++ mygeopy/tests/test_triangle.py
@@ -1,6 +1,7 @@
+import pytest
 from mygeopy.triangle import hypot
 def test_hypot():
-    assert hypot(3,4) == 3
+    assert hypot(3,4) == 5
     #assert hypot(0,0) == 0, "Non-zero inputs"
     #assert hypot(-1,1) == None, "NaN output"
    # assert hypot(3,4) == 5

ed0fa9349ca0438f040d4258d551e833474cdb55
jiasquaredfu <jjnaturalist@gmail.com>
2025-08-14 14:43:17 -0800

    add another ex

--- mygeopy/tests/test_triangle.py
+++ mygeopy/tests/test_triangle.py
@@ -1,8 +1,9 @@
 import pytest
 from mygeopy.triangle import hypot
+
 def test_hypot():
     assert hypot(3,4) == 5
     #assert hypot(0,0) == 0, "Non-zero inputs"
     #assert hypot(-1,1) == None, "NaN output"
    # assert hypot(3,4) == 5
-    #assert hypot(12,16) == 20
+    assert hypot(12,16) == 20
